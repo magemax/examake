a7d1a134c7585ceabb651e09a9cc065879518dfe
Augustin Wenger <magemax@users.noreply.github.com>
2024-02-03 17:30:25 +0100

    Exercice : Mesurer des angles donnés

---
+++ body_builders/mesurer_angles.py
@@ -0,0 +1,77 @@
+from template_management import load_template
+
+from random import randint, random, sample, choice
+
+from math import pi, sin, cos
+
+
+
+def angle_to_direction(angle, size= 7):
+    x=cos(angle)*size
+    y=sin(angle)*size
+    return (x,y)    
+
+def translate(point, direction, distance):
+    return tuple([point[i]+float(distance)*direction[i] for i in range(2)])
+
+
+def angle_random(): #Renvoie trois points du ABC, tels que l'angle ABC vaille un nombre entier
+# de degrés entre 1 et 179
+    a=(0,0)
+    rangle=random()*pi
+    b=angle_to_direction(rangle)
+    aangle2=randint(15,165)
+    rangle2=pi/180 * aangle2
+    c=angle_to_direction(rangle+rangle2)
+    return a,b,c, aangle2
+
+def generer_elements(nbl = 2, nbc = 1):
+    pictures=[]
+    nombre = nbl * nbc
+    cc= "|".join(nbc*"c")
+    tableau = [fr"\begin{{tabular}}{{{cc}}} \hline"] 
+    aangle2=-15
+    for i in range(nombre):
+        prevangle=aangle2
+        lettres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        rl = sample(lettres, 3)
+        dp = dict(zip("ABC", rl))
+        a,b,c=(0,-100),(0,100),(0,0)
+        ords=[k[1] for k in [a,b,c]]
+        
+        while max(ords)-min(ords)>9 or abs(aangle2-prevangle)<30:
+            a,b,c, aangle2 = angle_random()
+            ords=[k[1] for k in [a,b,c]]
+        coords={}
+        for k in range(3):
+            coords["ABC"[k]]=[a,b,c][k]
+        contenupicture = []
+        for point in "ABC":
+            contenupicture+=[fr"\coordinate ({point}) at {coords[point]};"]
+        contenupicture+=[fr'\draw pic[draw,fill=blue!30,angle radius=1cm, shift={{(6mm,3mm)}}] {{angle=B--A--C}};']
+        for point in "ABC":
+            contenupicture+=[f"\\draw plot[mark=x] coordinates{{{coords[point]}}} node[below] {{{dp[point]}}};"]
+        for couple in ["BA","AC"]:
+            dc = r"\draw "+str(coords[couple[0]])+" -- "+str(coords[couple[1]])+";"
+            contenupicture+=[dc]
+        nomangle = "".join([dp[k] for k in "BAC"])
+        contenupicture=[r"\begin{tabular}{c} \begin{tikzpicture}"]+contenupicture+[r"\end{tikzpicture}","\n"]
+        casephoto = "\n".join(contenupicture)
+
+        pictures+=["\n".join(contenupicture)]
+        tableau += [fr"{casephoto}" + "\\\\ \n" +fr"\Sicorrection{{$\widehat{{{nomangle}}}={aangle2}^\circ$}}{{$\widehat{{{nomangle}}}= $}} \end{{tabular}}"]
+        if i%nbc==nbc-1:
+            tableau +=[" \\\\ \\hline \n"]
+        else:
+            tableau +=" & "
+    tableau += [r"\end{tabular}"]
+    return {"tableau_triangle" : "\n".join(tableau)}
+
+def build(**args): 
+    texte_individuel=load_template(args["template"])
+    dico = generer_elements()
+
+    for k,v in dico.items():
+        texte_individuel=texte_individuel.replace("{w_"+k+"}",str(v))
+        
+    return(texte_individuel)
