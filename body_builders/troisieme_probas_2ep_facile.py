1f54a7115e018746892dfaea4cd6dea169cdec19
Augustin Wenger <magemax@users.noreply.github.com>
2024-03-09 15:07:31 +0100

    modification de typos dans la correctino de l'exo de probas facile

--- body_builders/troisieme_probas_2ep_facile.py
+++ body_builders/troisieme_probas_2ep_facile.py
@@ -22,7 +22,7 @@ def generer_elements():
     tot=a+b+c
 
     q = fr"""Dans le frigo, il y a {a+b+c} yaourts. {a} sont à la banane et {b} sont à l'abricot.\\
-    Lisa en choisit un au hasard. Son frère Victor en choisit un au hasard à son tour, et on s'intéresse aux yaourts obtenus les deux frères et soeurs.\\
+    Lisa en choisit un au hasard. Son frère Victor en choisit un au hasard à son tour, et on s'intéresse aux yaourts obtenus par les deux frères et soeurs.\\
     \textbf {{a.}}  Combien d'issues possède cette experience aléatoire ? Donner un exemple d'issue.\\
     \textbf {{b.}}  Est-ce une expérience en situation d'équiprobabilité ? Justifier.\\
     \textbf {{c.}}  Calculer la probabilité que Lisa et Victor aient choisi tous les deux un yaourt à la banane.\\
@@ -39,14 +39,14 @@ def generer_elements():
     Il y a donc $2\times2=4$ issues possibles.\\
     Par exemple : Lisa a pris un yaourt à la banane et Victor un yaourt à l'abricot. Ce qu'on peut noter (B,A).\\
     Les 9 issues sont : (B,B) (B,A) (A,B) (A,A) .\\
-    \textbf {{b.}}  Comme le nombre de yaourts est différent d'un parfum à l'autre, Lisa n'a pas la même probabilité de choisir n'importe quel parfum. On en déduit qu'il est impossible que les issues (B,B), (A,A) et (V,V) aient la même probabilité.\\
+    \textbf {{b.}}  Comme le nombre de yaourts est différent d'un parfum à l'autre, Lisa n'a pas la même probabilité de choisir n'importe quel parfum. On en déduit qu'il est impossible que les issues (B,B) et (A,A) aient la même probabilité.\\
     \textbf {{c.}}  Il y a {a} yaourts à la banane, et {tot} yaourts en tout, la probabilité que Lisa choisisse un yaourt à la banane est : ${fraction_to_latex(Fraction(a,tot))}$.\\
     Ensuite, il reste {a-1} yaourts à la banane pour Victor sur un total de {tot-1} yaourts.\\ 
     La probabilité qu'il choisisse à son tour et dans ces conditions ce parfum est : $\dfrac{{{a-1}}}{{{tot-1}}}={fraction_to_latex(Fraction(a-1,tot-1))}$.\\
     La probabilité de l'issue (B,B) est le produit de ces deux probabilités, donc : {fraction2epreuvessame(a,tot)}.\\
     \textbf {{d.}}  Les probabilités de  l'issue (A,A) peuvt être calculée de la même façon qu'à la question c) :\\{fraction2epreuvessame(b,tot)}.\\
     La probabilité qu'ils choisissent le même parfum est la somme des probabilités des issues (B,B) et (A,A), soit :\\
-    $\dfrac{{{a*(a-1)}}}{{{tot*(tot-1)}}}+\dfrac{{{b*(b-1)}}}{{{tot*(tot-1)}}}+\dfrac{{{b*(b-1)}}}{{{tot*(tot-1)}}}=\dfrac{a*(a-1) + b*(b-1)}{tot*(tot-1)}={fraction_to_latex(Fraction(a*(a-1) + b*(b-1) , tot*(tot-1)))} $.\\
+    $\dfrac{{{a*(a-1)}}}{{{tot*(tot-1)}}}+\dfrac{{{b*(b-1)}}}{{{tot*(tot-1)}}}+\dfrac{{{b*(b-1)}}}{{{tot*(tot-1)}}}=\dfrac{{{a*(a-1) + b*(b-1)}}}{{{tot*(tot-1)}}}={fraction_to_latex(Fraction(a*(a-1) + b*(b-1) , tot*(tot-1)))} $.\\
     \textbf {{e.}}  Choisir des parfums différents est l'événement contraire de l'événement dont on a calculé la probabilité à la question d).\\
     La probabilité de cet événement est donc : $1-{fraction_to_latex(Fraction(a*(a-1) + b*(b-1) + c*(c-1), tot*(tot-1)))}={fraction_to_latex(Fraction(tot*(tot-1) - (a*(a-1) + b*(b-1) + c*(c-1)), tot*(tot-1)))}$
     """
@@ -80,11 +80,11 @@ def generer_elements():
         return fr"{fraction_to_latex(Fraction(c, t1))}\times {fraction_to_latex(Fraction(g,t2))}={fraction_to_latex(Fraction(c*g, t1*t2))}"
 
     r = fr"""
-    \textbf {{a.}} Il y a {g} T-shirt rouges, sur {t2} T-shirts possibles. Comme Rémi fait son choix au hasard, les {t2} t-shirts sont équiprobables. La probabilité de choisir un t-shirt rouge est donc ${fraction_to_latex(Fraction(g,t2))}$
+    \textbf {{a.}} Il y a {g} T-shirt noirs, sur {t2} T-shirts possibles. Comme Rémi fait son choix au hasard, les {t2} t-shirts sont équiprobables. La probabilité de choisir un t-shirt noir est donc ${fraction_to_latex(Fraction(g,t2))}$
     \textbf {{b.}}  Il y a {a} paires de chaussettes rouges et il y a {t1} paires de chaussettes possibles.
-    La probabilité de choisir une paire de chaussettes blanches est : ${fraction_to_latex(Fraction(a, t1))}$.\\
-    Il y a {e} T-shirt rouges sur {t2} T-shirt possibles. La probabilité de choisir un des T-shirt bleu est : 
-    ${fraction_to_latex(Fraction(g,t2))}$.\\
+    La probabilité de choisir une paire de chaussettes rouges est : ${fraction_to_latex(Fraction(a, t1))}$.\\
+    Il y a {e} T-shirt rouges sur {t2} T-shirt possibles. La probabilité de choisir un des T-shirt rouges est : 
+    ${fraction_to_latex(Fraction(e,t2))}$.\\
     Jean-Claude a donc une probabilité de ${compose(a,e,t1,t2)}$ de choisir un t-shirt rouge et des chaussettes rouges.\\
     
     \textbf {{c.}}  La probabilité de choisir une paire de chaussettes vertes est : ${fraction_to_latex(Fraction(b, t1))}$ et la probabilité de choisir l'un des T-shirt verts est : ${fraction_to_latex(Fraction(f, t2))}$.\\
